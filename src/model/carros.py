caf9395c9728a0788ae6c1078628911a275d2559
barzifelipe <barzilaifelipe@gmail.com>
2025-10-20 10:05:18 -0300

    Fix model_carros

--- src/model/carros.py
+++ src/model/carros.py
@@ -1,11 +1,13 @@
 class Carro:
     def __init__(self,
                  id_carro:str=None,
+                 modelo:str=None,
                  placa:str=None,
                  categoria:str=None,
                  valor_diaria:float=None
                  ):
         self.set_id_carro(id_carro)
+        self.set_modelo(modelo)
         self.set_placa(placa)
         self.set_categoria(categoria)
         self.set_valor_diaria(valor_diaria)
@@ -13,6 +15,9 @@ class Carro:
     def set_id_carro(self, id_carro:str):
         self.id_carro = id_carro
     
+    def set_modelo(self, modelo:str):
+        self.modelo = modelo
+
     def set_placa(self, placa:str):
         self.placa = placa
     
@@ -25,6 +30,9 @@ class Carro:
     def get_id_carro(self) -> str:
         return self.id_carro
     
+    def get_modelo(self) -> str:
+        return self.modelo
+    
     def get_placa(self) -> str:
         return self.placa
     
@@ -35,4 +43,4 @@ class Carro:
         return self.valor_diaria
     
     def to_string(self):
-        return f"ID Carro: {self.get_id_carro()} | Placa: {self.get_placa()} | Categoria: {self.get_categoria()} | Valor da diária: {self.get_valor_diaria()}"
+        return f"ID Carro: {self.get_id_carro()} | Modelo: {self.get_modelo()} | Placa: {self.get_placa()} | Categoria: {self.get_categoria()} | Valor da diária: {self.get_valor_diaria()}"
